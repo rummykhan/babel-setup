178b34de5527d81fb109206b889d0fe14c5ca519
Rehan Manzoor <caprichano@gmail.com>
2017-02-10 02:26:11 +0400

    Modified code a bit

--- js/observer.js
+++ js/observer.js
@@ -3,18 +3,13 @@ module.exports = class Observer {
 
     constructor(){
         this.handlers = [];
-        console.log('constructor called');
+        console.log('constructor called..');
+        console.log('Do something stupid..');
     }
 
     attach(obj){
         this.handlers.push(obj)
     }
 
-    doSomething(obj){
-        console.log('do something');
-    }
 
-    justFunc(){
-        console.log('just a function');
-    }
-}
+};
